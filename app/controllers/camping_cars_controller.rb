1479f19d32ff40b0ab863180e4385ee2eddade76
david travailleur <travailleur.david@gmail.com>
2020-11-24 18:33:02 +0100

    setup controller owner + debut de la view

--- app/controllers/camping_cars_controller.rb
+++ app/controllers/camping_cars_controller.rb
@@ -7,22 +7,5 @@ class CampingCarsController < ApplicationController
     @camping_car = CampingCar.find(params[:id])
   end
 
-  def new
-    @camping_car = CampingCar.new
-  end
 
-  def create
-    @camping_car = CampingCar.new(camping_car_params)
-    if @camping_car.save
-      redirect_to  owner_camping_cars_path(@camping_car)
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def camping_car_params
-    params.require(:camping_car).permit(:user_id, :category, :title, :description, :brand, :sleeps_capacity, :price_per_day, :location, :availability)
-  end
 end
--- app/controllers/owner/camping_cars_controller.rb
+++ app/controllers/owner/camping_cars_controller.rb
@@ -1,2 +1,22 @@
 class Owner::CampingCarsController < ApplicationController
+
+  def new
+    @camping_car = CampingCar.new
+  end
+
+  def create
+    @camping_car = CampingCar.new(camping_car_params)
+    @camping_car.user = current_user
+    if @camping_car.save
+      redirect_to owner_camping_cars_path(@camping_car)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def camping_car_params
+    params.require(:camping_car).permit(:category, :title, :description, :brand, :sleeps_capacity, :price_per_day, :location, :availability, :photo)
+  end
 end
