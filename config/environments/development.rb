34a62c26428058fa9e9ee90c59f206820c7648ae
Yann Tarot <yann_tarot@yahoo.fr>
2020-11-24 12:52:45 +0100

    debugging cloudinary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  cloudinary:
-    service: Cloudinary
+
   config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
@@ -64,5 +63,3 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
-
-
