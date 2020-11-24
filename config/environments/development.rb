65f4fbbd8e8d36bd4f3834ee252a705f93b533cc
david travailleur <travailleur.david@gmail.com>
2020-11-24 12:35:42 +0100

    setup cloudinary clervercloud

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,9 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  cloudinary:
+    service: Cloudinary
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -62,3 +64,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+
