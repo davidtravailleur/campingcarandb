3cd83345f9be92ce549271a56e9e0441e242ce70
pierre groleau <groleau.pierre@gmail.com>
2020-11-26 14:51:07 +0100

    ajout du booking controller et de quelques modifs sur le show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,10 @@
 class BookingsController < ApplicationController
-  def new
-    @booking = Booking.new
-  end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
+    @booking.camping_car = CampingCar.find(params[:camping_car_id])
+    @booking.total_price = @booking.camping_car.price_per_day * (( @booking.end_date - @booking.start_date ).to_i)
     if @booking.save
       redirect_to owner_camping_cars_path
     else
@@ -13,13 +12,9 @@ class BookingsController < ApplicationController
     end
   end
 
-  def index
-    @bookings = current_user.camping_cars
-  end
-
   private
 
   def booking_params
-    params.require(@booking).permit(:start_date, :end_date, :total_price, :status)
+    params.require(@booking).permit(:start_date, :end_date, :camping_car_id)
   end
 end
