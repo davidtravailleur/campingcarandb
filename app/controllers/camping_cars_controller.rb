5ccd3909f6fb80612890d3324d3e45817333ba31
Yann Tarot <yann_tarot@yahoo.fr>
2020-11-27 14:31:35 +0100

    Refacto seeds and fixing searchform index

--- app/controllers/camping_cars_controller.rb
+++ app/controllers/camping_cars_controller.rb
@@ -3,8 +3,9 @@ class CampingCarsController < ApplicationController
   def index
     @camping_cars = CampingCar.all
     if params[:city] || params[:category]
-      @camping_cars = CampingCar.search_by_city_and_category(params[:city])
-
+      # raise
+      @camping_cars = CampingCar.where(location: params[:city].capitalize, category: params[:category])
+      # @camping_cars = CampingCar.where(location: params[:city].capitalize) && CampingCar.where(category: params[:category])
     end
 
     @markers = @camping_cars.geocoded.map do |camping_car|
@@ -13,9 +14,7 @@ class CampingCarsController < ApplicationController
         lng: camping_car.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { camping_car: camping_car }),
         image_url: helpers.asset_url('van-icone.jpg')
-
       }
-
     end
 
   end
