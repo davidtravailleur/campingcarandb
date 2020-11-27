3c3e6c4cd8774b70797cefd3e52bef519f41d567
pierre groleau <groleau.pierre@gmail.com>
2020-11-27 10:55:33 +0100

    Add flatpickr to campingcar show

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,11 +1,6 @@
 import flatpickr from "flatpickr";
-// import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 flatpickr(".datepicker", {
-  // minDate: "today",
-  // maxDate: new Date().fp_incr(365) // 14 days from now
-});
-
-// flatpickr({
-//   "plugins": [new rangePlugin({ input: "#secondRangeInput"})]
-// });
+    minDate: "today",
+    maxDate: new Date().fp_incr(365) // 14 days from now
+  });
